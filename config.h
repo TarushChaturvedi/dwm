4aa2f186d896121cd7900dc9a9e8a53cc7c39bd9
TarushChaturvedi <tarush.chaturvedi@gmail.com>
2025-01-08 16:36:22 +0530

    Add keybind for ksnip

--- config.h
+++ config.h
@@ -70,13 +70,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-
+static const char* ksnipcmd[] = { "ksnip", NULL };
 #include "shiftview.c"
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_s,	   spawn,          {.v = ksnipcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
